09141c3a097160f54de1337b8372709041311457
Ralle1976 <ralf.arnold@it-bfw.de>
2025-01-11 23:02:52 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,5 +4,5 @@ from .routes import api
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
-    app.register_blueprint(api, url_prefix='/api')
+    app.register_blueprint(api, url_prefix='/')  # Blueprint für die Haupt-URL registrieren
     return app
