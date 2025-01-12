2198c56afcd3c26fc0b40c005b15da1a3c568fa3
Ralle1976 <ralf.arnold@it-bfw.de>
2025-01-12 01:03:34 +0100

    Update gdrive_api.py

--- app/gdrive_api.py
+++ app/gdrive_api.py
@@ -1,11 +1,18 @@
+import os
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload
-import io
+from google.oauth2 import service_account
 
 def get_drive_service():
-    # Hier wird angenommen, dass die Google API Credentials bereits konfiguriert sind.
-    # Siehe: https://developers.google.com/drive/api/v3/quickstart/python
-    return build('drive', 'v3')
+    # Lese die JSON-Datei aus der Umgebungsvariable
+    credentials_info = os.getenv("GOOGLE_CREDENTIALS")
+    if not credentials_info:
+        raise RuntimeError("GOOGLE_CREDENTIALS environment variable is not set")
+
+    credentials = service_account.Credentials.from_service_account_info(
+        json.loads(credentials_info)
+    )
+    return build('drive', 'v3', credentials=credentials)
 
 def download_file_from_drive(service, file_id):
     request = service.files().get_media(fileId=file_id)
