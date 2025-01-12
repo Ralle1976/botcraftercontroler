9481989aa1b5385e014f0d623c5878a1db9862a3
Ralle1976 <ralf.arnold@it-bfw.de>
2025-01-12 02:18:46 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,22 @@
 import os
+import json
 
 class Config:
-    GITHUB_TOKEN = os.getenv('GITHUB_TOKEN', 'your_github_token')
-    GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID', 'your_google_client_id')
-    GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET', 'your_google_client_secret')
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
+    # Lädt die Konfiguration aus der Umgebungsvariable
+    GITHUB_REPO_CONFIG = json.loads(os.getenv("GITHUB_REPO_CONFIG", "{}"))
+
+    @staticmethod
+    def get_repo_config(repo_name):
+        """
+        Gibt die Konfiguration für ein bestimmtes Repository zurück.
+        :param repo_name: Der Name des Repositories (z. B. "repo1").
+        :return: Ein Wörterbuch mit URL und Token, falls vorhanden.
+        """
+        return Config.GITHUB_REPO_CONFIG.get(repo_name, None)
+
+    @staticmethod
+    def list_repos():
+        """
+        Gibt eine Liste aller verfügbaren Repository-Namen zurück.
+        """
+        return list(Config.GITHUB_REPO_CONFIG.keys())
